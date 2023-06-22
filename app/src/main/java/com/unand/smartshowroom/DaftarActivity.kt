7aa44358b0c9aa55468531b071661aed201ce170
id <uliaputra08@gmail.com>
2023-06-22 10:49:25 -0800

    login tess

--- app/src/main/java/com/unand/smartshowroom/DaftarActivity.kt
+++ app/src/main/java/com/unand/smartshowroom/DaftarActivity.kt
@@ -1,12 +1,17 @@
 package com.unand.smartshowroom
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.android.material.textfield.TextInputLayout
 import com.unand.smartshowroom.databinding.ActivityDaftarBinding
+import java.util.regex.Pattern
 
 class DaftarActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDaftarBinding
@@ -16,14 +21,28 @@ class DaftarActivity : AppCompatActivity() {
     private lateinit var nameEt: EditText
     private lateinit var passTil: TextInputLayout
     private lateinit var passEt: EditText
-    private lateinit var passKonfTil: TextInputLayout
-    private lateinit var passKonfEt: EditText
     private lateinit var registerButton: Button
+
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDaftarBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //init firebase auth
+        //handle back button click,
+        binding.backBtn.setOnClickListener {
+            onBackPressed()// goto previous scren
+        }
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.loginTextView.setOnClickListener {
+           val intent = Intent(this, LoginActivity::class.java)
+           startActivity(intent)
+       }
 
         emailTil = findViewById(R.id.emailTil)
         emailEt = findViewById(R.id.emailEt)
@@ -31,23 +50,52 @@ class DaftarActivity : AppCompatActivity() {
         nameEt = findViewById(R.id.nameEt)
         passTil = findViewById(R.id.passTil)
         passEt = findViewById(R.id.passEt)
-        passKonfTil = findViewById(R.id.passKonfTil)
-        passKonfEt = findViewById(R.id.passKonfEt)
         registerButton = findViewById(R.id.registerButton)
 
-
-
-
         binding.registerButton.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            val email = binding.emailEt.text.toString()
+            val name = binding.nameEt.text.toString()
+            val password = binding.passEt.text.toString()
+
+            //validasi email
+            if (email.isEmpty()){
+                binding.emailEt.error = "Email harus Diisi"
+                binding.emailEt.requestFocus()
+                return@setOnClickListener
+            }
+            //validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.emailEt.error = "Email Tidak Valid"
+                binding.emailEt.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi password
+            if (password.isEmpty()){
+                binding.passEt.error = "Kata Sandi harus Diisi"
+                binding.passEt.requestFocus()
+                return@setOnClickListener
+            }
+            //validasi panjang password
+            if (password.length < 8){
+                binding.passEt.error = "Kata Sandi Minimal 8 Karakter"
+                binding.passEt.requestFocus()
+                return@setOnClickListener
+            }
+            DaftarFirebase(email,name,password)
         }
+    }
 
-        //handle back button click,
-        binding.backBtn.setOnClickListener {
-            onBackPressed()// goto previous scren
-        }
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
-
-
+    private fun DaftarFirebase(email: String, name: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Daftar berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/unand/smartshowroom/LoginActivity.kt
+++ app/src/main/java/com/unand/smartshowroom/LoginActivity.kt
@@ -3,9 +3,18 @@ package com.unand.smartshowroom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
 import com.unand.smartshowroom.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -15,26 +24,122 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var tilPassword: TextInputLayout
     private lateinit var etPassword: EditText
     private lateinit var btnLogin: Button
+    private lateinit var btnFacebook: Button
+    private lateinit var btnGoogle: Button
+    private lateinit var btnApple: Button
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         tilUsername = findViewById(R.id.tilUsername)
         etUsername = findViewById(R.id.etUsername)
         tilPassword = findViewById(R.id.tilPassword)
         etPassword = findViewById(R.id.etPassword)
         btnLogin = findViewById(R.id.btnLogin)
+        btnFacebook = findViewById(R.id.btnFacebook)
+        btnGoogle = findViewById(R.id.btnGoogle)
+        btnApple = findViewById(R.id.btnApple)
 
         //handle back button click,
         binding.backBtn.setOnClickListener {
             onBackPressed()// goto previous scren
         }
 
-        binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
+
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+
+        //Bagian Login dengan Google
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        btnGoogle.setOnClickListener {
+            signInWithGoogle()
+        }
+
+        //login dengan email dan password
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            //validasi email
+            if (email.isEmpty()){
+                binding.etUsername.error = "Email harus Diisi"
+                binding.etUsername.requestFocus()
+                return@setOnClickListener
+            }
+            //validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.etUsername.error = "Email Tidak Valid"
+                binding.etUsername.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi password
+            if (password.isEmpty()){
+                binding.etPassword.error = "Kata Sandi harus Diisi"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+            //validasi panjang password
+            if (password.length < 8){
+                binding.etPassword.error = "Kata Sandi Minimal 8 Karakter"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+            LoginFirebase(email,password)
+        }
+    }
+
+    private fun LoginFirebase(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Selamat Datang $email", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Login berhasil, arahkan ke MainActivity
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+            startActivity(Intent (this, MainActivity::class.java))
+
+            finish() // Optional: Untuk menutup LoginActivity agar tidak dapat kembali dengan tombol back
+        } catch (e: ApiException) {
+            // Login gagal, tangani kesalahan
+            // ...
+        }
+    }
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
